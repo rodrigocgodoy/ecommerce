d664b260cd42f93186ee40c8b2e4d15f6e64303e
Rodrigo <rodrigocgodoy@hotmail.com>
2019-01-21 23:10:07 -0200

    Removendo arquivos de configuração e criando classe page

--- vendor/hcodebr/php-classes/src/DB/Sql.php
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Hcode\DB;
 
@@ -15,7 +15,7 @@ class Sql {
 	{
 
 		$this->conn = new \PDO(
-			"mysql:dbname=".Sql::DBNAME.";host=".Sql::HOSTNAME, 
+			"mysql:dbname=".Sql::DBNAME.";host=".Sql::HOSTNAME,
 			Sql::USERNAME,
 			Sql::PASSWORD
 		);
@@ -26,7 +26,7 @@ class Sql {
 	{
 
 		foreach ($parameters as $key => $value) {
-			
+
 			$this->bindParam($statement, $key, $value);
 
 		}
