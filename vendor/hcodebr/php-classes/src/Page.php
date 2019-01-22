e4813faef95172af066ae2167ca7c10a8801d2c9
Rodrigo <rodrigocgodoy@hotmail.com>
2019-01-22 00:49:32 -0200

    Erro 'fread(): Length parameter must be greater than 0' file 'C:\ecommerce\vendor\rain\raintpl\library\Rain\Tpl.php' line '237'

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -6,18 +6,18 @@ use Rain\Tpl;
 
 class Page {
 
-  private $tpl;
-  private $options = [];
-  private $defaults = [
-      "data"=>[]
-  ];
+    private $tpl;
+    private $options = [];
+    private $defaults = [
+        "data"=>[]
+    ];
 
-    public function __construct($opts = array()) {
+    public function __construct($opts = array(), $tpl_dir = "/views/") {
 
         $this->options = array_merge($this->defaults, $opts);
 
         $config = array(
-            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
             "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
             "debug"         => false // set to false to improve the speed
         );
